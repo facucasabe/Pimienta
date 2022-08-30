43ae167debebe94278e52ad1315ba61f33f7e149
fcasabella <fcasabella@DESKTOP-Q7CEK5O.localdomain>
2022-08-30 01:40:20 -0300

    back

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const routerApi = require('./routes')
+const cors = require('cors');
 const { errorHandler, logErrors } = require('./middlewares/error.handler')
 
 const app = express();
 
 const port = 3001
 
+
 app.use(express.json())
+
+const whitelist = ['http://localhost:3000'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
+app.use(cors(options));
+
 routerApi(app)
 
 app.use(logErrors)
